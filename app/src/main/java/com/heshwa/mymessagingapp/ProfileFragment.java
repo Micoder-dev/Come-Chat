a8b098bf9c5b2576591af7a44a0320d2c7f9967f
Micoder-dev <vetrimaran1439@gmail.com>
2022-04-12 22:31:35 +0530

    UI updated

--- app/src/main/java/com/heshwa/mymessagingapp/ProfileFragment.java
+++ app/src/main/java/com/heshwa/mymessagingapp/ProfileFragment.java
@@ -9,8 +9,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,15 +17,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import android.widget.Button;
 import android.content.Intent;
-import java.util.HashMap;
-import com.google.firebase.database.ServerValue;
 import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
-
-import androidx.appcompat.app.AlertDialog;
-import android.content.DialogInterface;
 import android.widget.Toast;
 
 
@@ -73,9 +65,10 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
         circleMenu=view.findViewById(R.id.circle_menu);
 
-        circleMenu.setMainMenu(Color.parseColor("#CDCDCD"),R.drawable.menu_icon,R.drawable.cancel_icon)
+        circleMenu.setMainMenu(Color.parseColor("#FF69B4"),R.drawable.menu_icon,R.drawable.cancel_icon)
                 .addSubMenu(Color.parseColor("#88bef5"),R.drawable.home_icon)
                 .addSubMenu(Color.parseColor("#83e85a"),R.drawable.search_icon)
                 .addSubMenu(Color.parseColor("#FF4832"),R.drawable.notification_icon)
@@ -87,6 +80,7 @@ public class ProfileFragment extends Fragment {
                         switch (index){
                             case 0:
                                 Toast.makeText(getActivity(),"Home",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getActivity(),MainActivity.class));
                                 break;
                             case 1:
                                 Toast.makeText(getActivity(),"Search",Toast.LENGTH_SHORT).show();
@@ -96,6 +90,7 @@ public class ProfileFragment extends Fragment {
                                 break;
                             case 3:
                                 Toast.makeText(getActivity(),"Settings",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getActivity(),SettingsActivity.class));
                                 break;
                             case 4:
                                 Toast.makeText(getActivity(),"GPS",Toast.LENGTH_SHORT).show();
