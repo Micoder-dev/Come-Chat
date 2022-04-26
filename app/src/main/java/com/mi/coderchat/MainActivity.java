b25625e02f9dbc852db126254b1549d07e688643
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-04-26 22:04:01 +0530

    ads are now muted while it's playings

--- app/src/main/java/com/mi/coderchat/MainActivity.java
+++ app/src/main/java/com/mi/coderchat/MainActivity.java
@@ -15,6 +15,9 @@ import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.tabs.TabLayout;
@@ -51,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
+
+        // Set app volume to be half of current device volume.  MobileAds.setAppVolume(0.5f);
+        MobileAds.setAppVolume(0.0f);
+        //To inform the SDK that the app volume has been muted, use the setAppMuted() method:
+        MobileAds.setAppMuted(true);
 
         txtdiplayUserName = findViewById(R.id.txtDisplayName);
 
