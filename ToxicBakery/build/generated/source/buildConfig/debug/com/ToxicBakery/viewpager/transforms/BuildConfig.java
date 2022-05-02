31e5aa245ec6e352e3541605f7b42068670ba6a5
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-02 18:23:47 +0530

    fixed sdk location error in Android Studio

---
+++ ToxicBakery/build/generated/source/buildConfig/debug/com/ToxicBakery/viewpager/transforms/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ToxicBakery.viewpager.transforms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.ToxicBakery.viewpager.transforms";
+  public static final String BUILD_TYPE = "debug";
+}
